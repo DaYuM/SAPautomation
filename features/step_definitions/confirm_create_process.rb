df420fa5187b741eb7b754a408c0ecf0cac6d249
William M Tinnes <william.tinnes@lego.com>
2017-05-01 13:53:06 -0400

    updates

---
+++ features/step_definitions/confirm_create_process.rb
@@ -0,0 +1,66 @@
+
+
+Given(/^I am on SAP CRM/) do
+  pending
+end
+
+When(/^I attempt to create an account without a last name$/) do
+  pending
+end
+
+Then(/^I should see the error message "([^"]*)"$/) do |arg|
+  pending
+end
+
+
+When(/^I attempt to create an account without a country$/) do
+  pending
+end
+
+When(/^I attempt to search for an Account using its specific AccountNumber$/) do
+  pending
+end
+
+Then(/^I should be able to see all of the consumer's address information$/) do
+  pending
+end
+
+And(/^I click new$/) do
+  pending
+end
+
+And(/^I enter first name$/) do
+  pending
+end
+
+And(/^I enter last name$/) do
+  pending
+end
+
+And(/^I enter Address$/) do
+  pending
+end
+
+And(/^I enter e\-mail address$/) do
+  pending
+end
+
+When(/^I click save$/) do
+  pending
+end
+
+Then(/^I have an account number$/) do
+  pending
+end
+
+When(/^I click accept$/) do
+  pending
+end
+
+Then(/^I should see a consumers account$/) do
+  pending
+end
+
+Then(/^I should see the standard consumers account$/) do
+  pending
+end
