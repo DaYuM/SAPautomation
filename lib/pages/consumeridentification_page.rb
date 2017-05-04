0f091db2e8b88747f74546f9afc06a49d0d5ff81
William M Tinnes <william.tinnes@lego.com>
2017-05-04 13:41:30 -0400

    updates

---
+++ lib/pages/consumeridentification_page.rb
@@ -0,0 +1,27 @@
+
+
+class ConsumerIdentificationPage
+  include PageObject
+  include DataMagic
+  include ErrorPanel
+
+
+
+  class "th-menu2-item"
+
+  Brickopedia id="C7_W28_V29_Y_BRICKOPE"
+  Consumer Identification id="C6_W28_v29_IC_BPIDENT"
+  Consumer Overview id="C6_W28_V29_IC_BP_ACC"
+  Account Fact Sheet id="C6_W28_V29_YIC_AFS"
+  Sales Overview id="C6_W28_V29_YSLS-CYC-W"
+  Sales Overview Arrow
+  class = "th-menu2-arrow"
+  class ="th-menu2-sub-item"
+                     Search Sales Order id="C6_W28_V29_SLS_ERS-SR"
+                     Create Sales Order id="C6_W28_V29_SLS_ERS-CR"
+                     Search Contract id="C6_W28_V29_SLS_ERC-SR"
+                     Create Contract id="C6_W28_V29_SLS_ERC-CR"
+
+
+
+end
