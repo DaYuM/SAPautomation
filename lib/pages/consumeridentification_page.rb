5ff765ec2285ab291514f25759625cc4bbc3cd10
William M Tinnes <william.tinnes@lego.com>
2017-05-05 09:59:39 -0400

    updates

--- lib/pages/consumeridentification_page.rb
+++ lib/pages/consumeridentification_page.rb
@@ -5,7 +5,7 @@ class ConsumerIdentificationPage
   include DataMagic
   include ErrorPanel
 
-
+#Navigation bar customer confirmed
 
   class "th-menu2-item"
 
@@ -25,3 +25,33 @@ class ConsumerIdentificationPage
 
 
 end
+
+#Account Search Fields
+    Customer Number: id="C3_W18_V19_V20_searchcustomer_struct.partner"
+    First Name: id="C3_W18_V19_V20_searchcustomer_struct.mc.name2"
+    Last Name: id="C3_W18_V19_V20_searchcustomer_struct.mc.name1"
+    Street/House Number:id="C3_W18_V19_V20_searchcustomer_struct.street"
+    City: id="C3_W18_V19_V20_searchcustomer_struct.city1"
+    Postal Code: id="C3_W18_V19_V20_searchcustomer_struct.post_code1"
+    Region: id="C3_W18_V19_V20_searchcustomer_struct.region"
+    Country: id="C3_W18_V19_V20_searchcustomer_struct.country"
+    Telephone: id="C3_W18_V19_V20_searchcustomer_struct.telephone"
+    Fax: id="C3_W18_V19_V20_searchcustomer_struct.fax"
+    E-mail Address: id="C3_W18_V19_V20_searchcustomer_struct.email"
+    LEGO ID: id="C3_W18_V19_V20_searchcustomer.yylego_username"
+    VIP Card: id="C3_W18_V19_V20_searchcustomer.yylego_rewcard"
+    Order number: id="C3_W18_V19_V20_searchcustomer_ext.yyord_num"
+    Club contract: id="C3_W18_V19_V20_searchcustomer_ext.yyclub_con"
+    Consumer Request: id="C3_W18_V19_V20_searchorder_struct.object_id"
+    Chat ID: id="C3_W18_V19_V20_searchcustomer_struct.yy_chat_id"
+    Search Account: id="C3_W18_V19_V20_Search"
+    Reset Fields: id="C3_W18_V19_V20_Reset"
+    New Account: id="C3_W18_V19_V20_New"
+
+#upon Account searched
+    Confirm
+    Save
+    Delete
+    Edit
+    New Search
+    Mobil
